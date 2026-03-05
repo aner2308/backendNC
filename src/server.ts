f6cc519dba7e46c8d80bb49133a9b26cdd56495c
Anton <erikssonanton94@gmail.com>
2026-03-05 11:38:38 +0100

    Grundstruktur för nytt backend-projekt skapad

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("NextChapter API körs");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("Kopplad till MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server körs på port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
